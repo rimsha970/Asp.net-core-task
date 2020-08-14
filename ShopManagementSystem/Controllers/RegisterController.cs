2eed111a2c2851c8c08d8b3c440f3658f91e927a
rimsha07 <rimsha_shahzad985@ymail.com>
2020-08-14 22:47:50 +0500

    All views are created along with login functionality

---
+++ ShopManagementSystem/Controllers/RegisterController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopManagementSystem.Models;
+
+namespace ShopManagementSystem.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ConnectionDBClass _db;
+
+        public RegisterController(ConnectionDBClass db)
+        {
+            _db = db;
+        }
+        // GET: RegisterController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RegisterController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegisterController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegisterController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Register uc)
+        {
+            _db.Add(uc);
+            _db.SaveChanges();
+            ViewBag.message = "The user " + uc.Name + " is saved successfully";
+            return RedirectToAction("User", "Login");
+        }
+
+        // GET: RegisterController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegisterController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegisterController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegisterController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ShopManagementSystem/Models/ConnectionDBClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopManagementSystem.Models
+{
+    public class ConnectionDBClass: DbContext
+    {
+        public ConnectionDBClass(DbContextOptions<ConnectionDBClass> options)
+            : base(options)
+        { }
+        public DbSet<Register> Login { get; set; }
+    }
+}
